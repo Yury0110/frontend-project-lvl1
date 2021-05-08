dca6f6c8d3e7a580c7344c5e4a9bac79937a1eac
Yuriy Grigorev <niceninjabox@gmail.com>
2021-05-08 19:14:14 +1000

    update parity.js

--- bin/src/parity.js
+++ bin/src/parity.js
@@ -1,5 +1,5 @@
 console.log(Answer "yes" if the number is even, otherwise answer "no".);
 
-const parity = () => {
-    console.log(`Question: ${Math.random}`);
-}
+const parity = console.log(`Question: ${console.log(Math.floor(Math.random() * 100) + 1);}`);
+
+export default parity;
