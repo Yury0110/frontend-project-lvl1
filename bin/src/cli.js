2251b1bcc383b39bdb5440ff5b7140fafe657b73
Yuriy Grigorev <niceninjabox@gmail.com>
2021-05-12 18:36:58 +1000

    add consts for answers

--- bin/src/cli.js
+++ bin/src/cli.js
@@ -1,15 +1,14 @@
 import readlineSync from 'readline-sync';
 
-console.log('Welcome to the Brain Games!');
-
-const name = readlineSync.question('May I have your name?');
+let name;
+export const askName = () => { name = readlineSync.question('May I have your name?'); };
 
 const greeting = `Hello, ${name}!`;
 
 export default greeting;
 
-export const rightAnswer = 'Correct!'; 
-export const wrongAnswer1 = `'yes' is wrong answer ;(. Correct answer was 'no'. \n Let's try again, ${name}!`;
-export const wrongAnswer2 = `'no' is wrong answer ;(. Correct answer was 'yes'. \n Let's try again, ${name}!`;
-export const congrat = `Congratulations ${name}!`;
-export const incorrectAnswer = 'Incorrect answer!';
+export const rightAnswer = () => 'Correct!';
+export const yesIsWrongAnswer = () => `'yes' is wrong answer ;(. Correct answer was 'no'. \nLet's try again, ${name}!`;
+export const noIsWrongAnswer = () => `'no' is wrong answer ;(. Correct answer was 'yes'. \nLet's try again, ${name}!`;
+export const congrat = () => `Congratulations ${name}!`;
+export const incorrectAnswer = () => 'Incorrect answer!';
