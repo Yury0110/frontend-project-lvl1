fae71f368b316ad5f450c9cc732c292c0573822d
Yuriy Grigorev <niceninjabox@gmail.com>
2021-05-12 17:47:40 +1000

    make consts for answers

--- bin/src/cli.js
+++ bin/src/cli.js
@@ -5,4 +5,11 @@ console.log('Welcome to the Brain Games!');
 const name = readlineSync.question('May I have your name?');
 
 const greeting = `Hello, ${name}!`;
+
 export default greeting;
+
+export const rightAnswer = 'Correct!'; 
+export const wrongAnswer1 = `'yes' is wrong answer ;(. Correct answer was 'no'. \n Let's try again, ${name}!`;
+export const wrongAnswer2 = `'no' is wrong answer ;(. Correct answer was 'yes'. \n Let's try again, ${name}!`;
+export const congrat = `Congratulations ${name}!`;
+export const incorrectAnswer = 'Incorrect answer!';
