d73b22fb8a31ce9075f0a6dcf38184b0084ca7ed
Yuriy Grigorev <niceninjabox@gmail.com>
2021-05-12 17:48:52 +1000

    d

--- bin/src/parity.js
+++
@@ -1,5 +0,0 @@
-console.log(Answer "yes" if the number is even, otherwise answer "no".);
-
-const parity = console.log(`Question: ${console.log(Math.floor(Math.random() * 100) + 1);}`);
-
-export default parity;
