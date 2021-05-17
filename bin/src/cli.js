51425b8d3b9ce5f5ff527d995df72d067c8061d7
Yuriy Grigorev <niceninjabox@gmail.com>
2021-05-17 15:46:56 +1000

    add letsTryAgain function

--- bin/src/cli.js
+++ bin/src/cli.js
@@ -3,12 +3,11 @@ import readlineSync from 'readline-sync';
 let name;
 export const askName = () => { name = readlineSync.question('May I have your name?'); };
 
-const greeting = `Hello, ${name}!`;
-
-export default greeting;
+export const greeting = () => console.log(`Hello, ${name}!`);
 
 export const rightAnswer = () => 'Correct!';
 export const yesIsWrongAnswer = () => `'yes' is wrong answer ;(. Correct answer was 'no'. \nLet's try again, ${name}!`;
 export const noIsWrongAnswer = () => `'no' is wrong answer ;(. Correct answer was 'yes'. \nLet's try again, ${name}!`;
-export const congrat = () => `Congratulations ${name}!`;
+export const congrat = () => `Congratulations, ${name}!`;
 export const incorrectAnswer = () => 'Incorrect answer!';
+export const letsTryAgain = () => `Let's try again, ${name}!`;
