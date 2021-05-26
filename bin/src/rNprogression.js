32a222a0d52b02a13e4eb0d7ee8f978a561f99f8
Yuriy Grigorev <niceninjabox@gmail.com>
2021-05-26 16:51:57 +1000

    generate arithmetic progression

---
+++ bin/src/rNprogression.js
@@ -0,0 +1,31 @@
+const randomNumber10 = () => Math.floor(Math.random() * 10) + 1;
+
+const randomNumber50 = () => Math.floor(Math.random() * 50) + 1;
+
+const getRandomArbitrary = (min, max) => Math.floor(Math.random() * (max - min) + min);
+
+const randomElement = getRandomArbitrary(2, 8);
+
+const arithProgr = () => {
+  const array = [];
+  const randNumb10 = randomNumber10();
+  const randNumb50 = randomNumber50();
+
+  for (let i = randNumb50; array.length < 10; i += randNumb10) {
+    array.push(i);
+  }
+  array[randomElement] = '..';
+  return array;
+};
+
+// const arr = arithProgr();
+
+
+
+// const getMissElement = () => {
+//   arr[randomElement] = '..';
+//   return arr;
+// };
+// console.log(getMissElement());
+
+export default arithProgr;
