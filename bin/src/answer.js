382ea85bd9019e251d20483141829da5a63cb995
Yuriy Grigorev <niceninjabox@gmail.com>
2021-06-18 22:02:26 +1000

    deleted excess

--- bin/src/answer.js
+++
@@ -1,5 +0,0 @@
-import readlineSync from 'readline-sync';
-
-const answer = () => readlineSync.question('Your answer:');
-
-export default answer;
--- bin/src/randomNumber.js
+++
@@ -1,5 +0,0 @@
-// import readlineSync from 'readline-sync';
-
-const randomNumber = () => Math.floor(Math.random() * 100) + 1;
-
-export default randomNumber;
