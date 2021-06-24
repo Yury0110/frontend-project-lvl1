bae7483dff5f42efb3064c349c5f24be0a64aaec
Yuriy Grigorev <niceninjabox@gmail.com>
2021-06-24 19:41:49 +1000

    update

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,5 @@
 #!/usr/bin/env node
 
-import { greeting, askName } from '../src/cli.js';
+import welcome from '../src/cli.js';
 
-console.log('Welcome to the Brain Games!');
-askName();
-greeting();
+welcome();
